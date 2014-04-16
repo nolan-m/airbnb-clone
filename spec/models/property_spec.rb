707a4e4c530a1a78457bae0a1ee69347a1c31407
Michael Grossman & Nolan Miller <student@epicodus.com>
2014-04-16 14:42:43 -0700

    add specs for relationships

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Property do
+  it { should belong_to :user }
+  it { should have_many :reservations }
   it { should validate_presence_of :title }
   it { should validate_presence_of :description }
   it { should validate_presence_of :city }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe User do
   it { should have_many :properties }
+  it { should have_many :reservations }
 end
