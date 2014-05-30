5aa11a4866a82889e79386dcf24056597656cc12
Nolan Miller <namiller2@gmail.com>
2014-05-29 20:19:38 -0700

    removed letter opener when emails are sent

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,8 @@ Airbnb::Application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries= false
 
 
   # Print deprecation notices to the Rails logger.
