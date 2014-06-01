d6e5862888f8c9b331c7964c50af62619e550a59
Nolan Miller <namiller2@gmail.com>
2014-06-01 12:28:15 -0700

    ammend

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,8 +2,7 @@ class Picture < ActiveRecord::Base
   belongs_to :property
 
   has_attached_file :image,
-  	 :styles => { small: "100x100>", medium: "250x250#", large: "640x420#" },
-  	 :url => ":s3_domain_url"
+  	 :styles => { small: "100x100>", medium: "250x250#", large: "640x420#" }
 
   validates_attachment_content_type :image, :content_type =>
   ["image/jpg", "image/jpeg", "image/png"]
